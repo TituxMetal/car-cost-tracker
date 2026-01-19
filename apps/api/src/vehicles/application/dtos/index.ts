461b237d2631467b2d66162dad16746c10dd5ff3
Titux Metal (DEV) <github@lgdweb.fr>
2026-01-19 18:29:11 +0100

    chore(vehicles): add barrel exports for DTOs and use cases
    - dtos/index.ts: export all DTOs
    - use-cases/index.ts: export CreateVehicle and GetVehicleByUser use cases

---
+++ apps/api/src/vehicles/application/dtos/index.ts
@@ -0,0 +1,4 @@
+export { CreateVehicleDto } from './CreateVehicle.dto'
+export { GetVehicleDto } from './GetVehicle.dto'
+export { UpdateMileageDto } from './UpdateMileage.dto'
+export { UpdateVehicleDto } from './UpdateVehicle.dto'
